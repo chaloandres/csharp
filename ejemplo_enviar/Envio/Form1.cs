c8a5813b1bf80e6b32d7426a276f2b14f55313e6
Andres Vergara <chalo.andres@live.cl>
2013-05-02 20:04:52 -0400

    Commit inicial codigos C#

---
+++ ejemplo_enviar/Envio/Form1.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Envio
+{
+    public delegate void Ejecutar(String var);
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Funcion(String var)
+        {
+            textBox1.Text = var;
+            DialogResult resultado = MessageBox.Show("Texto Enviado", "Alerta", MessageBoxButtons.YesNoCancel);
+
+            if (resultado == DialogResult.No)
+            {
+                MessageBox.Show("Reiniciar la aplicacion");
+                Application.Restart();
+            }
+            if (resultado == DialogResult.Cancel)
+            {
+                MessageBox.Show("Ingrese una opcion válida.","Sugerencia");
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            Form2 ventana = new Form2();
+            ventana.Envio = new Ejecutar(this.Funcion);
+            ventana.ShowDialog();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ejemplo_enviar/Envio/Form2.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Envio
+{
+    
+    public partial class Form2 : Form
+    {
+        public Ejecutar Envio;
+       
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Envio(textBox1.Text);
+            this.Close();
+            
+        }
+    }
+}
